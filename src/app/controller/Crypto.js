703596d4d5f704889be91e2961551390e807f3e4
Summy <s4594789@student.uq.edu.au>
2023-11-11 16:20:53 +1000

    api query by sortBy and order

--- src/app/controller/Crypto.js
+++ src/app/controller/Crypto.js
@@ -41,11 +41,24 @@ exports.fetchAllStats = async (req, res) => {
     let statsPromises = uniqueSymbols.map((symbol) =>
       fetchCurrencyStats(req, symbol, CryptoCurrencyModel)
     );
+
     let stats = await Promise.all(statsPromises);
     stats.sort((a, b) => b.marketCap - a.marketCap);
     stats.forEach((item, index) => {
       item.rank = index + 1;
     });
+
+    const { sortBy, order } = req.query;
+    if (sortBy && order) {
+      stats.sort((a, b) => {
+        if (order === "desc") {
+          return b[sortBy] - a[sortBy];
+        } else {
+          return a[sortBy] - b[sortBy];
+        }
+      });
+    }
+
     return res.status(status.OK).json(stats);
   } catch (error) {
     return res.status(status.INTERNAL_SERVER_ERROR).json({ error });
